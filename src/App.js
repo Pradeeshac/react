4ae48378a40a7935a3b6568acc03d423047d0641
PRADEESHA <111351400+Pradeeshac@users.noreply.github.com>
2023-11-19 22:01:50 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './components/label'
+
+
+
+function App() {
+  return (
+    
+       <div className="label">
+      <div className="text-wrapper">QR Code</div>
+      <div className="text-wrapper">Generator</div>
+    </div>
+     
+    
+  );
+
+  }
+export default App;
---
+++ src/components/label.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./label.css";
+
+
+
+export const Label = () => {
+  return (
+    <div className="label">
+      <div className="text-wrapper">QR Code</div>
+    </div>
+  );
+};
+export default Label;
